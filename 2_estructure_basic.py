76359afc4442e83abbb59798178b323a98bab872
Edson-Padilha <edsonp.bsi@gmail.com>
2024-03-19 09:23:09 -0300

    2_Estructure_basic

---
+++ 2_estructure_basic.py
@@ -0,0 +1,14 @@
+import flet as ft 
+
+def main(page: ft.Page):
+
+    pass
+
+# Aplication App
+ft.app(target=main)
+
+# Aplication web
+#ft.app(target=main, view=ft.AppView.WEB_BROWSER)
+
+# Especific port
+#ft.app(port=8550, target=main)
